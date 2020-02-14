62dd316c392907f0e744ec97b27f776f3fe31d2b
pxckets <49133592+pxckets@users.noreply.github.com>
2020-02-13 22:23:11 -0500

    Update

--- class.py
+++ class.py
@@ -3,7 +3,19 @@
 # This is the midterm for CIS-115, this program features 5 True/False and 5 MC questions, and automatically calculates a grade
 # CIS-115 Jan 23, 2020
 
+import sys
+import os
+from os import system, name  
 import time
+
+#Clear console function
+def clear(): 
+  
+    if name == "nt": 
+        _ = system("cls") 
+    else: 
+        _ = system("clear") 
+
 
 # Variables
 FirstName = input("What is your name?: ")
@@ -27,64 +39,155 @@ elapsed_time = None
 
 quiz_start = None
 
-#Timer I kinda stole
-class TimerError(Exception):
-    """A custom exception used to report errors in use of Timer class"""
+quiz_start = input("Are you ready to begin your attempt? Y/N: ")
 
-class Timer:
-    def __init__(self):
-        self._start_time = None
-
-    def start_timer(self):
-        if self._start_time is not None:
-            raise TimerError(f"Timer is running. Use .stop() to stop it")
-
-        self._start_time = time.perf_counter()
-
-    def stop_timer(self):
-        if self._start_time is None:
-            raise TimerError(f"Timer is not running. Use .start() to start it")
-
-        elapsed_time = time.perf_counter() - self._start_time
-        self._start_time = None
-        #print(f"Elapsed time: {elapsed_time:0.4f} seconds")
-
+if quiz_start == "Y":
+    quiz_start = True
+else:
+    quiz_start = False
 
 if quiz_start == True:
+
     #Quiz start
 
     print("Welcome to the CIS-115 midterm!")
     print("")
     print("Hope you studied")
-    quiz_start = input("Are you ready to begin your attempt? Y/N: ")
 
-    if quiz_start == "Y":
-        quiz_start = True
-    else:
-        quiz_start = False
-        
     #______________________________________________________________________________
 
     #Question 1
-
+    print("")
+    print("___________________________")
     print("Question 1: (True or False)")
     print("")
     print("CIS-115 is my favorite course?")
 
     Question1Answer = input("T/F: ")
 
-    if Question1Answer == "T" or "True":
+    if Question1Answer == "T":
+        print("")
+        print("")
         print("Correct")
+        print("")
+        print("")        
         TotalScore = TotalScore + 10
     else:
         print("Incorrect, CIS-115, is your favorite class")
         TotalScore = TotalScore + 0
 
+    print("Movng to question 2 in 5 seconds...")
+    time.sleep(5)
+    clear()
+
     #______________________________________________________________________________
 
     #Question 2
+    print("")
+    print("___________________________")
+    print("Question 2: (True or False)")
+    print("")
+    print("All humans are mammals")
 
-    print("Question 2: (Multiple choice)")
+    Question2Answer = input("T/F: ")
+
+    if Question2Answer == "T":
+        print("")
+        print("")
+        print("Correct")
+        print("")
+        print("")        
+        TotalScore = TotalScore + 10
+    else:
+        print("Incorrect, you must be a fish")
+        TotalScore = TotalScore + 0
+
+    print("Movng to question 3 in 5 seconds...")
+    time.sleep(5)
+    clear()
+    #______________________________________________________________________________
+
+    #Question 3
+    print("")
+    print("___________________________")
+    print("Question 3: (True or False)")
+    print("")
+    print("North Carolina is in the USA")
+
+    Question3Answer = input("T/F: ")
+
+    if Question3Answer == "T":
+        print("")
+        print("")
+        print("Correct")
+        print("")
+        print("")
+        TotalScore = TotalScore + 10
+    else:
+        print("Incorrect, you must be a canadian")
+        TotalScore = TotalScore + 0
+
+    print("Movng to question 4 in 5 seconds...")
+    time.sleep(5)
+    clear()
+
+    #______________________________________________________________________________
+
+    #Question 4
+    print("")
+    print("___________________________")
+    print("Question 1: (True or False)")
+    print("")
+    print("2 + 2 = 9121232")
+
+    Question1Answer = input("T/F: ")
+
+    if Question1Answer == "F":
+        print("")
+        print("")
+        print("Correct")
+        print("")
+        print("")        
+        TotalScore = TotalScore + 10
+    else:
+        print("Incorrect, you must be dyslexic")
+        TotalScore = TotalScore + 0
+
+    print("Movng to question 5 in 5 seconds...")
+    time.sleep(5)
+    clear()
+
+    #______________________________________________________________________________
+
+    #Question 5
+    print("")
+    print("___________________________")
+    print("Question 5: (True or False)")
+    print("")
+    print("The USA is fake")
+
+    Question3Answer = input("T/F: ")
+
+    if Question3Answer == "T":
+        print("")
+        print("")
+        print("Correct")
+        print("")
+        print("")
+        TotalScore = TotalScore + 10
+    else:
+        print("Incorrect, you must be a canadian")
+        TotalScore = TotalScore + 0
+
+    print("Movng to question 6 in 5 seconds...")
+    time.sleep(5)
+    clear()
+    #______________________________________________________________________________
+
+    #Question 6
+    print("")
+    print("___________________________")
+    print("Question 6: (Multiple choice)")
     print("")
     print("The language we are using is:")
     print("|(a) Python  |")
@@ -92,27 +195,37 @@ if quiz_start == True:
     print("|(c) Nerd    |")
     print("|(a) C++     |")
 
-    Question2Answer = input("Answer: ")
+    Question6Answer = input("Answer: ")
 
-    if Question2Answer == "a" or "Python":
+    if Question6Answer == "a" or "Python":
+        print("")
+        print("")
         print("Correct")
+        print("")
+        print("")        
         TotalScore = TotalScore + 10
     else:
         print("Incorrect, answer A, or Python, was the correct answer")
         TotalScore = TotalScore + 0
 
+    print("Movng to question 7 in 5 seconds...")
+    time.sleep(5)
+    clear()
     #______________________________________________________________________________
-
+    
     if TotalScore >= 90:
-        LetterGrade == "A"
+        LetterGrade = "A"
     elif TotalScore >= 80:
-        LetterGrade == "B"
+        LetterGrade = "B"
     elif TotalScore >= 70:
-        LetterGrade == "C"
+        LetterGrade = "C"
     elif TotalScore >= 60:
-        LetterGrade == "D"
+        LetterGrade = "D"
     else:
-        LetterGrade == "F"
+        LetterGrade = "F"
+
+
+    print(FirstName + ", your grade was a " + LetterGrade)
 
 else:
     print("Restart the program when you are ready")
